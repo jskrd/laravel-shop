18e6beeb5f3600dad8b9816f2063c46f44b5c0b6
Joe S <joe@skrd.uk>
2020-09-07 15:33:42 +0100

    Use single lines

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('addresses', 'AddressController')
-    ->only(['store', 'show', 'update', 'destroy']);
+Route::apiResource('addresses', 'AddressController')->only(['store', 'show', 'update', 'destroy']);
 
-Route::apiResource('baskets', 'BasketController')
-    ->only(['store', 'show', 'update', 'destroy']);
+Route::apiResource('baskets', 'BasketController')->only(['store', 'show', 'update', 'destroy']);
 
 Route::apiResource('baskets.variants', 'BasketVariantController');
 
@@ -16,13 +14,10 @@ Route::apiResource('discounts', 'DiscountController')->only(['show']);
 
 Route::apiResource('products', 'ProductController')->only(['index', 'show']);
 
-Route::apiResource('products.images', 'ProductImageController')
-    ->only(['index']);
+Route::apiResource('products.images', 'ProductImageController')->only(['index']);
 
-Route::apiResource('products.variants', 'ProductVariantController')
-    ->only(['index']);
+Route::apiResource('products.variants', 'ProductVariantController')->only(['index']);
 
 Route::apiResource('variants', 'VariantController')->only(['show']);
 
-Route::apiResource('variants.images', 'VariantImageController')
-    ->only(['index']);
+Route::apiResource('variants.images', 'VariantImageController')->only(['index']);
